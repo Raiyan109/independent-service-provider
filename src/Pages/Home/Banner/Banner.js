6145b25e0e905250f642f1caeaa1484684f62503
Raiyan109 <kraiyan109@gmail.com>
2022-04-17 00:49:36 +0600

    slider added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import './Banner.css'
+import banner1 from '../../../Images/banner/quran.jpg'
+import banner2 from '../../../Images/banner/aqeedah.jpg'
+import banner3 from '../../../Images/banner/arabic (2).jpg'
 
 const Banner = () => {
     return (
-        <div>
-            <h1>this is banner </h1>
-        </div>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner1}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3 className='banner-heading'>QURAN</h3>
+                    <p>At My RKK TUITION, students are taught the recitation of the Quraan with proper Tajweed rules step by step following the Saudi-Arabian Accent. Reciting Quran with tajweed gives 10 rewards for each letter recited. The reciter will be from the best of the people.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner2}
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3 className='banner-heading'>AQEEDAH</h3>
+                    <p>Illuminating the basic belief in Allah, His beautiful 99 names, discussing the matters which are known from the Quran and sound ahadeeth. On the whole, making clear what a Muslim must believe in his heart, by accepting the truth of Allah and His Messenger.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner3}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption >
+                    <h3 className='banner-heading'>ARABIC</h3>
+                    <p>Learning Arabic can be a long and rigorous road. We as Muslim ummah are commanded to understand and reflect upon the Holy Quran. To do so, students at RKK TUITION are taught Arabic language at different levels depending on the age and their ability to learn.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     );
 };
 
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import './Header.css'
-import logo from '../../../Images/attachment_95854975.png'
+import logo from '../../../Images/download.png'
+import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <>
-            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+            <Navbar collapseOnSelect expand="lg" bg="light" sticky='top' variant="light">
                 <Container>
-                    <Navbar.Brand href="#home">
+                    <Navbar.Brand as={Link} to="/">
                         <img src={logo} height='100' alt="" />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#features">Features</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
-                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                            <Nav.Link href="#features">Services</Nav.Link>
+                            <Nav.Link href="#pricing">Blogs </Nav.Link>
+                            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
+                            </NavDropdown> */}
                         </Nav>
                         <Nav>
-                            <Nav.Link href="#deets">More deets</Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                                Dank memes
+                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            <Nav.Link as={Link} to="/login">
+                                Login
                             </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
