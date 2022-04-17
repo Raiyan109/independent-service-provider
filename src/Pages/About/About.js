057eef43a6a2022ecce327254dd8a63b0a850a2f
Raiyan109 <kraiyan109@gmail.com>
2022-04-17 22:49:25 +0600

    about done

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,7 +3,12 @@ import './About.css'
 const About = () => {
     return (
         <div>
-            <h1>this is about </h1>
+            <h1>Raiyan Kabir Khan</h1>
+            <p>In The next 4 months, I will be a web developer</p>
+            <p>I will work hard</p>
+            <p>I will not waste my times. </p>
+            <p>I will do minimum 1 project every day after ending the course.</p>
+            <p>I will give 5-6 hours daily Inshallah.</p>
         </div>
     );
 };
