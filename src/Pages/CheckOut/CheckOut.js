8a565ba7892516f116da035a583d7b5c1b8d6c84
Raiyan109 <kraiyan109@gmail.com>
2023-01-03 17:01:37 +0600

    checkout working

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './CheckOut.css'
+import BreadcrumbExample from '../BreadCrumb';
 const CheckOut = () => {
     return (
         <div>
+            <BreadcrumbExample />
             <h1>Please CheckOut</h1>
         </div>
     );
