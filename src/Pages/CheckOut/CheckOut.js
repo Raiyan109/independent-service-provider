d37ba316bf4d5fe200e1a4b697fadefbae4b7741
Raiyan109 <kraiyan109@gmail.com>
2023-02-28 15:17:40 +0600

    gradient background

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -3,9 +3,12 @@ import './CheckOut.css'
 import BreadcrumbExample from '../BreadCrumb';
 const CheckOut = () => {
     return (
-        <div>
+        <div className='checkout-bg'>
             <BreadcrumbExample />
-            <h1>Please CheckOut</h1>
+            <h1>Get started with an expert tutor today</h1>
+            <h5>Only pay for the tutoring time you request - no extra fees or commitments!
+                Choose to pay-as-you-go, OR save money by pre-purchasing one of our tutoring plans for a full semester (16 weeks) of tutoring.
+                Select the plan that’s right for you.</h5>
         </div>
     );
 };
