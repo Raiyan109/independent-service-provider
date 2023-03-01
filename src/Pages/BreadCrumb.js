fb5053b28a195322b8c6ef67de2454283106d773
Raiyan109 <kraiyan109@gmail.com>
2023-03-01 22:25:37 +0600

    stripe started

--- src/Pages/BreadCrumb.js
+++ src/Pages/BreadCrumb.js
@@ -1,14 +1,32 @@
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import { Link, useLocation } from 'react-router-dom';
 
 function BreadcrumbExample() {
+    const location = useLocation()
+    console.log(location);
+
+    let currentLink = ''
+
+    const crumbs = location.pathname.split('/')
+
+        .filter(crumb => crumb !== '')
+        .map(crumb => {
+            currentLink = + `/${crumb}`
+
+            return (
+                <div className='crumb' key={crumb}>
+                    <Link to={currentLink}>{crumb}</Link>
+                </div>
+            )
+        })
     return (
-        <Breadcrumb>
-            <Breadcrumb.Item href="/" className=''>Home</Breadcrumb.Item>
-            <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
-                Library
-            </Breadcrumb.Item>
-            <Breadcrumb.Item active>Data</Breadcrumb.Item>
-        </Breadcrumb>
+        // <Breadcrumb>
+        //     <Breadcrumb.Item href="/" className=''>Home</Breadcrumb.Item>
+        //     <Breadcrumb.Item active>Data</Breadcrumb.Item>
+        // </Breadcrumb>
+        <div className='bread-crumbs'>
+            {crumbs}
+        </div>
     );
 }
 
--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -2,15 +2,24 @@ import React from 'react';
 import './CheckOut.css'
 import BreadcrumbExample from '../BreadCrumb';
 import Pricing from '../Pricing';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
+console.log(stripePromise);
 const CheckOut = () => {
     return (
         <div className='checkout-bg'>
-            <BreadcrumbExample />
-            <h1>Get started with an expert tutor today</h1>
-            <h6>Only pay for the tutoring time you request - no extra fees or commitments!
-                Choose to pay-as-you-go, OR save money by pre-purchasing one of our tutoring plans for a full semester (16 weeks) of tutoring.
-                Select the plan that’s right for you.</h6>
+            <div style={{ marginBottom: '90px', }}>
+                {/* <BreadcrumbExample /> */}
+                <h1>Get started with an expert tutor today</h1>
+                <div style={{ margin: 'auto' }}>
+                    <h6 style={{ textAlign: 'center' }}>Only pay for the tutoring time you request - no extra fees or commitments!
+                        Choose to pay-as-you-go, OR save money by pre-purchasing one of our tutoring plans for a full semester (16 weeks) of tutoring.
+                        Select the plan that&sol;s right for you.</h6>
 
+                </div>
+
+            </div>
             <Pricing />
         </div>
     );
